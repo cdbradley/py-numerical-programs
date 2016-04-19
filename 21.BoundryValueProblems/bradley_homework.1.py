02d2e1d432bf55d0a7a55fe15aaf8a41f935a811
cdbradley <coltbradley@gmail.com>
2016-04-19 12:28:23 -0400

    added .tex and asociated files, also added graphs at various relaxation thresholds

--- 21.BoundryValueProblems/bradley_homework.1.py
+++ 21.BoundryValueProblems/bradley_homework.1.py
@@ -14,29 +14,35 @@ import pylab as py
 
 #define values
 k=5.
-f_0=0.
-f_f=2.
-N=100
+x_0=0.
+x_f=1.
+y_0=0.
+y_f=2.
+N=100.
 change = 10.
 ch=[]
 steps=0
-h = (f_f-f_0)/N
+h = (x_f-x_0)/N
 X=np.linspace(0,1,100)
 Y=np.zeros(N)
-Y[0]=f_0
-Y[-1]=f_f
+Y[0]=y_0
+Y[-1]=y_f
 for i in range(len(Y+1))[1:-1]:
     Y[i]=Y[i-1]+h
 ch.append(Y[50])
 
-while change>.00000000001:    
+while change>.0000001:    
     Y[1:-1] = .5*(Y[:-2]+Y[2:])-(k*h**2)/2.*np.sqrt(1+((Y[:-2]-Y[2:])**2)/(2*h)**2)
     ch.append(Y[50])
     change = abs(ch[-1]-ch[-2])
-    steps=+1
+    steps= steps+1
 
 print steps
 
 py.close()
 py.plot(X,Y,"b")
+py.title("String relaxation with .0000001 error allowed")
+py.xlabel("X-Distance (m)",fontsize = 16)
+py.ylabel("Y-Distance (m)",fontsize = 16)
+py.savefig("1_relaxation(.0000001).png")
 py.show()
