f9812b818bf2a0869c77fe236e3b4c61c323b94a
cdbradley <coltbradley@gmail.com>
2016-04-22 00:16:33 -0400

    created homework 1 and 2 files for lesson 22

---
+++ 22.PDEs/bradley_homework.2.py
@@ -0,0 +1,61 @@
+# Solve the PDE for waves on a string
+import pylab as pl
+import numpy as np
+
+# Parameters
+c = 50.0
+xa = -5.0
+xb = 5.0
+A = 0.1
+sigma = 1.0
+
+# Set up grid
+N = 100  # number of space intervals
+nts = 200  # number of timesteps
+plt = 20 # number of timesteps between plots
+dx = (xb - xa)/N
+dt = 0.25*dx/c
+cdtoverdxsqr = (c*dt/dx)**2
+x = np.linspace(xa,xb,N+1)
+
+# arrays
+y = np.zeros(N+1)
+ym = np.zeros(N+1) # "y_minus" = y at previous timestep
+yp = np.zeros(N+1) # "y_plus"  = y at next timestep
+v = np.zeros(N+1)
+vm = np.zeros(N+1) # "v_minus" = v at previous timestep
+vp = np.zeros(N+1) # "v_plus"  = v at next timestep
+
+# initial data
+for i in range(0,N+1,1):
+    ym[i] = A*np.exp(-(x[i]/sigma)**2)
+pl.close()
+pl.plot(x,ym)
+
+# take the first timestep
+y = np.copy(ym)
+n = 1
+time = dt
+
+# Evolve
+while n < nts:
+    for i in range(1,N,1):
+        #first step
+        ym[i] = y[i] +(dt/2)*v[i]
+        vm[i] = v[i] +(dt/2)*c**2*((y[i+1]-2*y[i]+y[i-1])/dx**2)
+        #second step
+        yp[i] = y[i]+dt* v[i]
+        vp[i] = v[i] +(dt/2)*c**2*((ym[i+1]-2*ym[i]+ym[i-1])/dx**2)
+        
+        
+    ym = np.copy(y)
+    y = np.copy(yp)
+    n = n + 1
+    time = time + dt
+    if (plt*(n/plt) == n): #plot every plt timesteps
+        pl.plot(x,y)
+        print n, time
+
+pl.xlabel('x')
+pl.ylabel('y')
+pl.show()
