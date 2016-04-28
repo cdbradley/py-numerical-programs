1d9d08f73ae78b8ae407526c15243f63a8c8038d
cdbradley <coltbradley@gmail.com>
2016-04-28 12:52:15 -0400

    created final exam folder/files

---
+++ Final/bradley_final1.py
@@ -0,0 +1,8 @@
+#Colt Bradley
+#4.28.16
+#PY251 Final
+
+#import modules
+import numpy as np
+import pylab as py
+
--- general_functions.py
+++ general_functions.py
@@ -2,3 +2,29 @@
 #General Functions
 
 #import necessary modules
+import numpy as np
+import pylab as py
+
+def RK2_2ndOrderODE(f,a0=0,b0=0,ti=0,tf=10,ts=100):
+    a = a0
+    b = b0
+    dt = (tf-ti)/ts
+    A = []
+    B = []
+    time = []
+    t = 0
+    
+    for i in range(0, ts):
+        #do first step
+        a1 = a + b*dt/2
+        b1 = b + f(a)*dt/2
+        
+        #do second step
+        a = a + b1*dt
+        b = b + f(a1)*dt
+        
+        #interate values and append solutions to a list
+        t = t +dt
+        return A.append(a)
+        return B.append(b)
+        return time.append(t)
